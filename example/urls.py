c74c3404d20fd358345bac8b010713e7fd19a90a
vyshnav <vyshnavofc@gmail.com>
2024-01-23 19:56:17 +0530

    local navbar

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,23 @@
-# example/urls.py
 from django.urls import path
-
-from example.views import index
-
+from . import views
+app_name="example"
 
 urlpatterns = [
-    path('', index),
+    path('', views.index, name='index'),
+    # path('200HoursYogaTeachersTrainingCourse', views.twohundred, name='200HoursYogaTeachersTrainingCourse'),
+    # path('300HoursYogaTeachersTrainingCourse', views.threehundred, name='300HoursYogaTeachersTrainingCourse'),
+    # path('YogaPhilosophyTeachersTraining', views.philosophy, name='YogaPhilosophyTeachersTraining'),
+    # path('ChakraHealerCourse', views.chakra, name='ChakraHealerCourse'),
+    # path('AncientCosmicEnergyHealingProcess', views.cosmic, name='AncientCosmicEnergyHealingProcess'),
+    # path('PrenatalandPostnatalYogaTTC', views.natal, name='PrenatalandPostnatalYogaTTC'),
+    # path('whatsapp', views.whatsapp, name='whatsapp'),
+    # path('test', views.test, name='test'),
+    # path('instructor', views.instructor, name='instructor'),
+    # path('Become_A_Healer', views.Become_A_Healer, name='Become_A_Healer'),
+    # path('Spiritual_Retreat', views.Spiritual_Retreat, name='Spiritual_Retreat'),
+    # path('Contact', views.Contact, name='Contact'),
+    # path('sitemap', views.sitemap, name='sitemap'),
+    # path('robot', views.robot, name='robot'),
+
 ]
+
--- example/views.py
+++ example/views.py
@@ -1,16 +1,6 @@
-# example/views.py
-from datetime import datetime
+from django.shortcuts import render
+from django.shortcuts import redirect
 
-from django.http import HttpResponse
-
+# Create your views here.
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, "index.html")
